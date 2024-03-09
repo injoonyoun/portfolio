b6b9bdf3b377b29f60b8d7ccb01158684d7113c4
Injoon <injoon.youn@gmail.com>
2024-03-09 15:55:26 -0500

    create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Shop from './routes/shop/shop.component';
+import About from './routes/about/about.component';
+import WorkOne from './routes/work1/work1.components';
+import WorkTwo from './routes/work2/work2.components';
+import WorkThree from './routes/work3/work3.components';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='about' element={<About />} />
+        <Route path='work/1' element={<WorkOne />} />
+        <Route path='work/2' element={<WorkTwo />} />
+        <Route path='work/3' element={<WorkThree />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
