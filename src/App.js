7a0cb338850e60877509e7fb05c6627fc3eb6f6d
Injoon <injoon.youn@gmail.com>
2024-03-11 23:24:32 -0400

    edit routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Carousel from './components/carousel/carousel.component';
+import Grid from './components/grid/grid.component';
+import Spiral from './components/spiral/spiral.component';
 import Navigation from './routes/navigation/navigation.component';
 import Shop from './routes/shop/shop.component';
 import About from './routes/about/about.component';
@@ -12,7 +15,10 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path="/carousel" component={Carousel} />
+        <Route path="/grid" component={Grid} />
+        <Route path="/spiral" component={Spiral} />
         <Route path='shop' element={<Shop />} />
         <Route path='about' element={<About />} />
         <Route path='work/1' element={<WorkOne />} />
